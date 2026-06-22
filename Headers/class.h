0161e32b5c35970f3e06909b453f715eaee17d6e
YourFavorite <lesoli8i7i5@gmail.com>
2026-06-22 14:23:26 +0300

    trying to use switch, vector

--- Headers/class.h
+++ Headers/class.h
@@ -9,4 +9,7 @@ class Registration {
     public:
         Registration(std::string Newname, std::string Newemail, std::string Newpassword);
         void displayInfo();
+        void ChangeRegistration(); //need to add in near future
+        void DeleteRegistration(); //NTAINF
+        
 };
--- main.cpp
+++ main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
+#include <vector>
 #include "Headers/class.h"
 
 int main() {
     bool exit = false;
+    std::vector<std::string> NamesOfRegistaration;
+
+    std::cout<<"Hello!\nWhat do you want ot do?\n";
+    
     while (exit == false) {
         //Menu
-        std::cout<<"Hello!\n What do you want ot do?\n";
+        
         std::cout<<"1.Create new application\n";
         std::cout<<"2.Change application\n";
         std::cout<<"3.Delete application\n";
-        std::cout<<"4.Exit\n";
+        std::cout<<"4.Display information about application";
+        std::cout<<"5.Exit\n\n";
         std::cout<<"What you pick:\n";
         //Picking an action
         int Pick;
         std::cin>>Pick;
         
         switch(Pick) {
-            case 1: Registration first();
+            case 1: 
+                Registration first();
+                break;
+            case 2:
+                break; 
+            case 3: 
+                break;
+            case 4:
+                break;
+            case 5:
+                exit = false;
+                break;
+            default:
+                std::cout<<"Enter valid number\n";
+                break;
         }
 
         /*
